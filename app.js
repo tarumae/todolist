209d058884027701091152417cf24ff8167f4e66
BleakMouse <tom.arumae@gmail.com>
2020-05-10 16:19:16 +0100

    Fix styling

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-tom:bigdataenergy@cluster0-kcs7p.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("<- Redacted ->", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = {
   name: String
